b865492800a0fca50c55fe19dd3b3dffe09a09ae
Flavio <flavio_125@hotmail.com>
2026-06-15 17:11:39 -0300

    Corrigindo hash senha admin inicial

--- src/main/resources/db/migration/V025__corrigir_hash_da_senha_usuario_admin.sql
+++ src/main/resources/db/migration/V025__corrigir_hash_da_senha_usuario_admin.sql
@@ -1,3 +1,6 @@
+-- =====================================================================
+-- Ajustes
+-- =====================================================================
 UPDATE usuario 
-SET senha = '$2a$10$wR6Cby9b8GshbWOfV3HwUuNq3Xv2A176M.wI6bC2wQ9R1bA6fN7m6' 
+SET senha = '$$2a$10$hTPHF6GXQRD8qI5zaHx3de.vlyIsgp01RXCv1EjZlI84iApZrlPBC' 
 WHERE login = 'admin';
